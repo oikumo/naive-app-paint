5861c0752fd368dee4711ee3a1f7fd2e7410bffa
oikumo <oikumo@gmail.com>
2020-06-02 00:38:56 -0400

    Added brush color switch

--- src/run.mjs
+++ src/run.mjs
@@ -1,4 +1,4 @@
-import { CanvasRenderer, RenderTexture, uiComponent } from 'naive-3d'
+import { CanvasRenderer, RenderTexture, ui, createRectFromCorners } from 'naive-3d'
 import { paint, paintCanvas, paintColor } from './paint-canvas/paint-canvas.mjs'
 
 const interval = 5
@@ -7,44 +7,64 @@ const run = () => {
   const canvas = document.getElementById("canvas")
   const canvasRender = new CanvasRenderer(canvas)
   const renderTex = new RenderTexture(canvasRender.imageSize())
-  let bgColor = 0xFF474a43
+  const bgColor = 0xFF474a43
+  const red = 0xFF0000FF
+  const green = 0xFF00FF00
+  const blue = 0xFFFF0000
+  const gray = 0xFF111111
+
+  const { createUiComponent, drawUiComponent } = ui
+  const rect = createRectFromCorners({ x: 0, y: 100 }, { x: canvas.width, y: 0 })
+  const rectButton = createRectFromCorners({ x: 0, y: 150 }, { x: 100, y: 100 })
+
+  const panel = createUiComponent(rect, { x: 0, y: canvas.height - 100 }, gray)
+  const buttonRed = createUiComponent(rectButton, { x: 100, y: canvas.height - 100 }, red)
+  const buttonGreen = createUiComponent(rectButton, { x: 300, y: canvas.height - 100 }, green)
+  const buttonBlue = createUiComponent(rectButton, { x: 500, y: canvas.height - 100 }, blue)
+
+  const drawUi = () => {
+    drawUiComponent(panel, renderTex.texture, canvas.width)
+    drawUiComponent(buttonRed, renderTex.texture, canvas.width)
+    drawUiComponent(buttonGreen, renderTex.texture, canvas.width)
+    drawUiComponent(buttonBlue, renderTex.texture, canvas.width)
+  }
+
   let painting = false
-  const red = 0xFFFF0000
-  const green = 0xFF00FF77
   let currentColor = red
   paintColor(currentColor)
 
-  const changeColor = () => {
-    currentColor = currentColor === red ? green : red
-    paintColor(currentColor)
-  }
-
   canvas.onmousemove = ({ x, y }) => {
-    if (painting) paint(x, y)
+    if (painting) {
+      paint(x, y)
+    }
   }
 
-  canvas.onmousedown = () => painting = true
-  canvas.onmouseup = () => {
-    changeColor()
+  canvas.onmousedown = () => { painting = true }
+  canvas.onmouseup = ({ x, y }) => {
     painting = false
-  }
 
-  const background = () => uiComponent(0xFFFF5555, renderTex.texture, canvas.width, 0, canvas.height - 200, canvas.width, 200)
-  const button = (color, x, y, w, h) => uiComponent(color, renderTex.texture, canvas.width, x, y, w, h)
+    if (buttonRed.inside({ x, y })) {
+      paintColor(currentColor)
+      currentColor = red
+      return
+    }
 
-  const buttonA = () => button(0xFF00FF77, (canvas.width / 2) - 100, canvas.height - 100, 200, 50)
-  const buttonB = () => button(0xFFFF0000, (canvas.width / 2) - 100, canvas.height - 150, 200, 50)
-
-  const ui = () => {
-    background()
-    buttonA()
-    buttonB()
+    if (buttonGreen.inside({ x, y })) {
+      currentColor = green
+      paintColor(currentColor)
+      return
+    }
+    if (buttonBlue.inside({ x, y })) {
+      paintColor(currentColor)
+      currentColor = blue
+      return
+    }
   }
 
   setInterval(() => {
     renderTex.clear(bgColor)
     paintCanvas(renderTex.texture, canvas.width)
-    ui()
+    drawUi()
     canvasRender.draw(renderTex.buf8)
   }, interval)
 }
