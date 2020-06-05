318a30a5612ef22d02b1ef557b18ef9b75f16e40
oikumo <oikumo@gmail.com>
2020-06-04 21:52:53 -0400

    Updated naive-3d and naive-tests versions

--- tests/paint-canvas/paint-canvas-test.mjs
+++ tests/paint-canvas/paint-canvas-test.mjs
@@ -1,5 +1,17 @@
-import { equals, test } from 'naive-tests'
+import { assertions, test } from 'naive-tests'
+import { paint } from '../../src/ui/paint-canvas.mjs'
 
-test('paint into a texture', () => { 
-    equals(true, true)
+test('paint into a texture', () => {
+    /*
+    const shape = (x, y) => {
+
+    }
+
+    const brush = new Brush(shape)
+    const textureWidth = 50
+    const textureHeight = 40
+    const texture = new Uint32Array(textureWidth * textureHeight)
+    brush.paintTo(texture, textureWidth)
+
+    equals()*/
 })
